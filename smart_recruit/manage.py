f251a8af050ce32bffacdaded556f6a448bbc32c
Inzamamul-huq <inzamulhuq0@gmail.com>
2025-08-24 22:38:03 +0530

    fixed

--- smart_recruit/manage.py
+++ smart_recruit/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_recruit.smart_recruit.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_recruit.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
